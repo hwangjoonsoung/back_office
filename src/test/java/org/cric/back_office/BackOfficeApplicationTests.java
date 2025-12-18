4926bce3e985526721c6b4be86d2b36df638985c
hwangjoonsoung <joonsoung93@naver.com>
2025-12-18 16:02:35 +0900

    init project

---
+++ src/test/java/org/cric/back_office/BackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cric.back_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
